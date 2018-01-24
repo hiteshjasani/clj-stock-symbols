421b2bb3ff1e2898ae5d217cdc484cb57ae01a1e
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-01-24 09:35:20 -0500

    initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-stock-symbols
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_stock_symbols/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-stock-symbols.core-test
+  (:require [clojure.test :refer :all]
+            [clj-stock-symbols.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
