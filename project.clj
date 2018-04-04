4bae27fd758ebfd47cc061152fe4b6155d0c0d24
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-04-03 21:31:26 -0400

    Prep for 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.jasani/clj-stock-symbols "0.1.1-SNAPSHOT"
+(defproject org.jasani/clj-stock-symbols "0.1.1"
   :description "Library of public US company stock symbols"
   :url "https://github.com/hiteshjasani/clj-stock-symbols"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,4 @@
                  [org.clojure/spec.alpha "0.1.123"]
                  [org.clojure/data.csv "0.1.4"]
                  [camel-snake-kebab "0.4.0"]]
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :sign-releases false}
-                        ]])
+  )
